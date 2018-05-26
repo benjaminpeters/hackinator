932f51bb4d0e134fe5e8d964075fd40d7e3ee68d
Ben Peters <benpeters49@gmail.com>
2018-05-25 20:41:00 -0400

    added hacker new api call

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -11,7 +11,7 @@ const NewsItem = (props) => {
           </a>
           <div>
             <h2>
-              <a href="">
+              <a href={props.url}>
                 {props.title} <em>&nbsp;</em>
               </a>
             </h2>
@@ -27,10 +27,40 @@ const NewsItem = (props) => {
 
 class Lists extends Component {
 
+  constructor(props) {
+   super(props);
+   this.state = {
+     stories:[]
+   }
+  }
+  componentDidMount(){
+   const topStories ='https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty';
+   const storyUrlBase ='https://hacker-news.firebaseio.com/v0/item/';
+
+   fetch(topStories)
+   .then(data=>data.json())
+   .then(data=>data.map(id=>{
+     const url=`${storyUrlBase}${id}.json`;
+     return fetch(url).then(d=>d.json()); //return array
+   }))
+   .then(promises=>Promise.all(promises))
+   .then(stories=>this.setState({stories}));
+  }
+
   render() {
+    let views = <div>Loading...</div>;
+    const {stories}=this.state;
+    if(stories &&  (stories.length>0)){
+      views=stories.map(s=>(
+        <div className="col-12 col-lg-6" key={s.id}>
+          <NewsItem count={s.score} url={s.url} comments={s.descendants} time={s.time} title={s.title} author={"coloneltcb"} />
+        </div>
+      ));
+    }
+
     return (
-      <div className="col-12 col-lg-6">
-        <NewsItem count={203} comments={139} time={8} title={"Andy Rubin Puts Essential Up for Sale, Cancels Next Phone (bloomberg.com)"} author={"coloneltcb"} />
+      <div>
+        {views}
       </div>
     );
   }
